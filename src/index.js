ef4e0d6c07bea80849a048c43dfd326140520954
BITTU THAKUR <125640492+bk-thakur@users.noreply.github.com>
2025-11-05 14:39:09 +0530

    Create initial React app structure in index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React + Docker + Jenkins + AWS ECR</h1>
+      <p>This app is successfully running from a Docker container!</p>
+    </div>
+  );
+}
+
+export default App;
